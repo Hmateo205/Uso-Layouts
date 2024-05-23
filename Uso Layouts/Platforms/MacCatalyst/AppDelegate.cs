2572070c89132ed58d9c911cdfd512dc0c33c0f3
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-22 20:22:05 -0500

    Scroll

---
+++ Uso Layouts/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Uso_Layouts
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
