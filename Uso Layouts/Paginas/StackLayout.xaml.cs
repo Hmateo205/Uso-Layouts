26f77f49a7832f0dc85d17c9404337a9d1f51809
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-23 00:41:42 -0500

    FlexLayout

--- Uso Layouts/Paginas/StackLayout.xaml.cs
+++ Uso Layouts/Paginas/StackLayout.xaml.cs
@@ -2,8 +2,12 @@ namespace Uso_Layouts.Paginas;
 
 public partial class StackLayout : ContentPage
 {
-	public StackLayout()
-	{
-		InitializeComponent();
-	}
+    public StackLayout()
+    {
+        InitializeComponent();
+    }
+    private void OnFlexLayoutClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Paginas.FlexLayout());
+    }
 }
