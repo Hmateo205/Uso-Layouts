45cae41159427da0885155c50ffda3d9246e5c50
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-23 01:00:39 -0500

    flex

--- Uso Layouts/Paginas/FlexLayout.xaml.cs
+++ Uso Layouts/Paginas/FlexLayout.xaml.cs
@@ -6,4 +6,9 @@ public partial class FlexLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    public async void Click(object sender, TappedEventArgs e)
+    {
+        await Navigation.PushAsync(new AbsoluteLayout());
+    }
 }
